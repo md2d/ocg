15efbfdc1d322fe441df19cfdc15f42f6f74b28c
d1ms <dimedrol@li.ru>
2023-03-04 05:49:03 +0300

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,20 @@
+/* v.1.0 - by md2d */
+const allowedPaths = ['catalog/product'];
+
+const initLoader = () => {
+  
+  let checkUrl = false;
+  
+  allowedPaths.forEach(
+    (url) => {
+      if( !checkUrl && location.href.indexOf(url) > 0 )
+        checkUrl = true;
+    }
+    
+    if(!checkUrl) 
+      return '';
+  );
+  
+}
+
+initLoader();
