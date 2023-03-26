3746dfe10cf4c5fc8125a8b0a3d7f6601ddcb267
d1ms <dimedrol@li.ru>
2023-03-26 13:03:17 +0300

    Update loader.js

--- loader.js
+++ loader.js
@@ -280,7 +280,7 @@ const initLoader = () => {
 	
 	let over = document.createElement('div');
 	over.id = 'overdiv';
-	over.innerHTML = '<i class="fa-5x fa fa-spinner"></i><style>.fa-spinner {animation: spin 5s infinite linear;-webkit-animation: spin2 5s infinite linear;}@keyframes spin {from {transform: scale(1) rotate(0deg);}to {transform: scale(1) rotate(360deg);}}@-webkit-keyframes spin2 {from {-webkit-transform: rotate(0deg);}to {-webkit-transform: rotate(360deg);}}.chatgptseo-refresh{float: right;right: 15px;position: absolute;top: 0;border-radius: 5px;}</style>';
+	over.innerHTML = '<i class="fa-5x fa fa-spinner"></i><style>.fa-spinner {animation: spin 5s infinite linear;-webkit-animation: spin2 5s infinite linear;}@keyframes spin {from {transform: scale(1) rotate(0deg);}to {transform: scale(1) rotate(360deg);}}@-webkit-keyframes spin2 {from {-webkit-transform: rotate(0deg);}to {-webkit-transform: rotate(360deg);}}.chatgptseo-refresh{float: right;right: 15px;position: absolute;top: 0;border-radius: 5px;z-index:1;}</style>';
 	$(over).css({
 		'display':'none',
 		'position':'fixed',
